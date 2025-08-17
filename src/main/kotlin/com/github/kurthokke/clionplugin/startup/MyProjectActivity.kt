df2d54526be0b03418e0a5ad91928243e9de62b7
GitHub Action <action@github.com>
2025-08-17 12:28:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/kurthokke/clionplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/kurthokke/clionplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.kurthokke.clionplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
